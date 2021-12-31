39ccb16dc49401ddfcaf53a7e5e66ded199909aa
Max Tung <max.tung.3@gmail.com>
2021-12-31 16:13:00 -0500

    Added scraping so that the real dollars spent shows in the budgets page. Somewhat hacked in there right now, but works.

--- toggle.js
+++ toggle.js
@@ -1,1 +1,19 @@
-document.body.style.border = "10px solid red";
+const BUDGET_LIST_ID = "spendingBudget-list-body"
+
+function adjustBudget(list_item) {
+	let real_dollars = list_item.getElementsByClassName("rollover-text")[0].children[3].textContent;
+	let display_div = list_item.children[1].children[0];
+	if (real_dollars && display_div.tagName === "DIV") {
+		var spent = display_div.children[0];
+		var newSpent = spent.cloneNode(true);
+		newSpent.id = "new-spent";
+		newSpent.children[1].textContent = "[" + real_dollars + "] ";
+		spent.before(newSpent);
+	}
+}
+
+let list = document.getElementById(BUDGET_LIST_ID);
+
+for (let i = 0; i < list.children.length; i++) {
+	adjustBudget(list.children[i])
+}
